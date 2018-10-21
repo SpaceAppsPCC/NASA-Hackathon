63ee0cc840a8b9fff8befee2e6041ce11cf41368
KossBoii <long.truong0320@gmail.com>
2018-10-20 20:49:59 -0700

    PollutionCrawler
    pollution parser based on the given latitude and longitude

--- server-side/Pollution.py
+++ server-side/Pollution.py
@@ -6,12 +6,10 @@ def openURL(url):
     Opens the argument url using urllib.request.urlopen
     Returns a dictionary created from the JSON data from the Launch Library Reading API
     :param url: string that contains the current url need to be read
-    :return: a dictionary parsed from the JSON data from the Launch Library Reading API
-    """
+    :return: a dictionary parsed from the JSON data from the Launch Library Reading API        """
 
     #check if you can open the URL.  passes back errors if URL does not open properly
-    try:
-        jsonFile = urllib.request.urlopen(url)
+    try:            jsonFile = urllib.request.urlopen(url)
     except urllib.error.HTTPError as e:
         print(e.code)
     except urllib.error.URLError as e:
@@ -24,8 +22,7 @@ def openURL(url):
 
     return parsed_json
 
-def parseLaunch():
-
+def locationID():
     # Initial URL in string that we will use to get the first launch JSON data after 2018-10-20
     launchURL = "https://launchlibrary.net/1.4/launch/2018-10-20?offset=0"
 
@@ -35,8 +32,6 @@ def parseLaunch():
     # calls openURL(url) to get a format we can read in python
     # currently, returns a dictionary that have list values
     parsed_json = openURL(launchURL)
-
-    print(launchURL)
 
     total = parsed_json['total']
 
@@ -56,44 +51,77 @@ def parseLaunch():
 
         # a new call of openURL(launchURL) for each 10 launches
         parsed_json = openURL(launchURL)
-        print(parsed_json)
 
         # Count keeps track of the total number of launches in the current JSON
         count = parsed_json['count']
 
         for i in range(count):
-            #currentCount keeps track of the current launch given from our API
+            # currentCount keeps track of the current launch given from our API
             currentCount = j * 10 + i + 1
 
-            #creates a dictionary for the current count
+            # creates a dictionary for the current count
             infoJson[currentCount] = {}
 
-            # The information we care about currently is the launch name, launch start time, launch location name, coordinates
-            # and mission description if applicable
-
-            infoJson[currentCount]['name'] = parsed_json['launches'][i]['name']
-            # print(parsed_json['launches'][i]['name'])
-            infoJson[currentCount]['launchStart'] = parsed_json['launches'][i]['windowstart']
-            # print(parsed_json['launches'][i]['windowstart'])
-            infoJson[currentCount]['launchEnd'] = parsed_json['launches'][i]['windowend']
-            # print(parsed_json['launches'][i]['location']['pads'][0]['name'])
-            infoJson[currentCount]['location'] = parsed_json['launches'][i]['location']['pads'][0]['name']
             # print(parsed_json['launches'][i]['location']['pads'][0]['latitude'])
             infoJson[currentCount]['latitude'] = parsed_json['launches'][i]['location']['pads'][0]['latitude']
             # print(parsed_json['launches'][i]['location']['pads'][0]['longitude'])
             infoJson[currentCount]['longitude'] = parsed_json['launches'][i]['location']['pads'][0]['longitude']
-
-            try:
-                print(parsed_json['launches'][i]['missions'][0]['description'])
-                infoJson[currentCount]['mission'] = parsed_json['launches'][i]['missions'][0]['description']
-            except IndexError:
-                infoJson[currentCount]['mission'] = 'No mission information provided'
-
         offset += 10
         launchURL = "https://launchlibrary.net/1.4/launch/2018-10-20?offset=%d"%offset
 
     return infoJson
 
+def aqiConvert(aqi):
+    if(0 <= aqi and aqi <= 50):
+        return "Good"
+    elif(51 <= aqi and aqi <=100):
+        return "Moderate"
+    elif(101 <= aqi and aqi <= 150):
+        return "Unhealthy for sensitive group"
+    elif(151 <= aqi and aqi <= 200):
+        return "Unhealthy"
+    elif(201 <= aqi and aqi <= 300):
+        return "Very unhealthy"
+    else:
+        return "Hazardous"
 
 
-parseLaunch()
+#Token key: c6ad742f8c8c04aabde6629b4346d77913923ce2
+def pollutionParser(dict):
+    infoJson = {}
+    for i in range(1,len(dict) + 1):
+        infoJson[i] = i
+        pollutionURL = "https://api.waqi.info/feed/"
+        pollutionURL += "geo:" + str(dict[i]['latitude']) + ";" + str(dict[i]['longitude']) + "/?token=" + "c6ad742f8c8c04aabde6629b4346d77913923ce2"
+
+        parsed_json = openURL(pollutionURL)
+
+        infoJson[i] = {}
+
+        infoJson[i]['airrate'] = str(parsed_json['data']['aqi'])
+        infoJson[i]['airquality'] = aqiConvert(parsed_json['data']['aqi'])
+        infoJson[i]['dominentpol'] = parsed_json['data']['dominentpol']
+        infoJson[i]['timemeasure'] = parsed_json['data']['time']['s']
+        infoJson[i]['timezone'] = parsed_json['data']['time']['tz']
+        infoJson[i]['station'] = parsed_json['data']['city']['name']
+        infoJson[i]['stationID'] = parsed_json['data']['idx']
+        infoJson[i]['latitude'] = str(parsed_json['data']['city']['geo'][0])
+        infoJson[i]['longitude'] = str(parsed_json['data']['city']['geo'][1])
+
+        print("i: " + str(i))
+        print(infoJson[i])
+        print()
+
+    return infoJson
+
+
+
+# dict = {1: {'latitude': 68.478, 'longtitude': 28.30123}, 2: {'latitude': 150, 'longtitude': 300}}
+dict = locationID();
+info = pollutionParser(dict)
+
+
+
+
+
+
